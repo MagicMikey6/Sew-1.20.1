4272b6ea3e4cb900852905035c87110f32906b96
MagicMikey6 <mikeycaruso2916@gmail.com>
2024-07-01 15:29:21 -0700

    Added New Item

--- src/main/java/net/magicmikey6/sow/Sow.java
+++ src/main/java/net/magicmikey6/sow/Sow.java
@@ -1,6 +1,7 @@
 package net.magicmikey6.sow;
 
 import com.mojang.logging.LogUtils;
+import net.magicmikey6.sow.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -16,13 +17,13 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Sow.MOD_ID)
 public class Sow {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "sow";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Sow() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
